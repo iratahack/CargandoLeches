c5673a4575d898f63f0dae7fbd882a0e4e917ffe
Craig Hackney <iratahack@digitalxfer.com>
2022-06-08 13:48:18 -0700

    Fixed compiler warnings and some cleanup.

--- src/CgLeches.c
+++ src/CgLeches.c
@@ -4,16 +4,17 @@
 #include <string.h>
 #include <libgen.h>
 
-FILE *fi, *fo, *ft;
 unsigned char in[0x10000], mem[0x20000];
-int i, j, k, tlength;
+int i, tlength;
 uint16_t length, frequency = 44100;
-char tzx = 0, wav = 0, channel_type = 1, velo = 3, offset = 0, command[0x100], *ext;
+char tzx = 0, channel_type = 1, velo = 3, offset = 0, command[0x100], *ext;
 
 int main(int argc, char *argv[])
 {
+    FILE *inFile, *outFile, *tmpFile;
     char *path = dirname(argv[0]);
-    if (argc == 1)
+
+    if ((argc < 3) || (argc > 7))
         printf("\n"
                "CgLeches v1.00, an ultra load block generator by Antonio Villena, 17 Sep 2014\n\n"
                "  CgLeches <ifile> <ofile> [speed] [channel_type] [srate] [offset]\n\n"
@@ -24,27 +25,30 @@ int main(int argc, char *argv[])
                "  [srate]        Sample rate, 44100 (default) or 48000\n"
                "  [offset]       -2,-1,0 (default),1 or 2. Fine grain adjust for symbol offset\n\n"),
             exit(0);
-    if (argc < 3 || argc > 7)
-        printf("\nInvalid number of parameters\n"),
+
+    if ((inFile = fopen(argv[1], "rb")) == NULL)
+        perror(argv[1]),
             exit(-1);
-    fi = fopen(argv[1], "rb");
-    if (!fi)
-        printf("\nInput file not found: %s\n", argv[1]),
+
+    fseek(inFile, 0, SEEK_END);
+    tlength = ftell(inFile);
+    fseek(inFile, 0, SEEK_SET);
+
+    if (!(ext = strrchr(argv[2], '.')))
+        printf("%s: Output file name must have .wav or .tzx extension\n", argv[2]),
             exit(-1);
-    fseek(fi, 0, SEEK_END);
-    tlength = ftell(fi);
-    fseek(fi, 0, SEEK_SET);
-    if (!(ext = strchr(argv[2], '.')))
-        printf("\nInvalid argument name: %s\n", argv[2]),
+
+    if ((outFile = fopen(argv[2], "wb+")) == NULL)
+        perror(argv[2]),
             exit(-1);
-    fo = fopen(argv[2], "wb+");
-    if (!fo)
-        printf("\nCannot create output file: %s\n", argv[2]),
-            exit(-1);
-    argc > 3 && (velo = atoi(argv[3]));
-    if (velo > 7)
-        printf("\nInvalid speed: %s\n", argv[3]),
-            exit(-1);
+
+    if (argc > 3)
+    {
+        velo = atoi(argv[3]);
+        if (velo > 7)
+            printf("\nInvalid speed: %s\n", argv[3]),
+                exit(-1);
+    }
     if (argc > 4)
     {
         if (!strcasecmp(argv[4], "mono"))
@@ -57,21 +61,31 @@ int main(int argc, char *argv[])
             printf("\nInvalid argument name: %s\n", argv[4]),
                 exit(-1);
     }
-    argc > 5 && (frequency = atoi(argv[5]));
-    if (frequency != 44100 && frequency != 48000)
-        printf("\nInvalid sample rate: %s\n", argv[5]),
-            exit(-1);
-    argc > 6 && (offset = atoi(argv[6]));
-    if (offset < -2 || offset > 2)
-        printf("\nInvalid offset: %s\n", argv[6]),
-            exit(-1);
-    if (!strcasecmp((char *)strchr(argv[2], '.'), ".tzx"))
-        fprintf(fo, "ZXTape!"),
-            *(uint32_t *)mem = 0xa011a,
-                       fwrite(mem, ++tzx, 3, fo);
-    else if (!strcasecmp((char *)strchr(argv[2], '.'), ".wav"))
+    if (argc > 5)
     {
-        memset(mem, wav++, 44);
+        frequency = atoi(argv[5]);
+        if (frequency != 44100 && frequency != 48000)
+            printf("\nInvalid sample rate: %s\n", argv[5]),
+                exit(-1);
+    }
+    if (argc > 6)
+    {
+        offset = atoi(argv[6]);
+        if (offset < -2 || offset > 2)
+            printf("\nInvalid offset: %s\n", argv[6]),
+                exit(-1);
+    }
+
+    if (!strcasecmp(ext, ".tzx"))
+    {
+        fprintf(outFile, "ZXTape!");
+        *(uint32_t *)mem = 0xa011a;
+        fwrite(mem, 1, 3, outFile);
+        tzx = 1;
+    }
+    else if (!strcasecmp(ext, ".wav"))
+    {
+        memset(mem, 0, 44);
         *(uint32_t *)mem = 0x46464952;
         *(uint32_t *)(mem + 8) = 0x45564157;
         *(uint32_t *)(mem + 12) = 0x20746d66;
@@ -82,19 +96,27 @@ int main(int argc, char *argv[])
         *(uint32_t *)(mem + 28) = frequency * (channel_type & 3);
         *(uint8_t *)(mem + 34) = 8;
         *(uint32_t *)(mem + 36) = 0x61746164;
-        fwrite(mem, 1, 44, fo);
+        fwrite(mem, 1, 44, outFile);
     }
     else
-        printf("\nInvalid file extension: %s\n", argv[2]),
-            exit(-1);
+    {
+        printf("%s: Invalid extension for output file name\n", ext);
+        exit(-1);
+    }
+
     ext++;
+
     while (tlength)
     {
-        fread(&length, 1, 2, fi);
-        fread(in, 1, length, fi);
-        ft = fopen("_tmp.tap", "wb+");
-        fwrite(in + 1, 1, length - 2, ft);
-        fclose(ft);
+        if (fread(&length, sizeof(length), 1, inFile) != 1)
+            perror("reading length"),
+                exit(-1);
+        if (fread(in, 1, length, inFile) != length)
+            printf("Error: Reading %d bytes\n", length),
+                exit(-1);
+        tmpFile = fopen("_tmp.tap", "wb+");
+        fwrite(in + 1, 1, length - 2, tmpFile);
+        fclose(tmpFile);
 #ifdef __linux__
         sprintf(command, "%s/leches %d %s tmp.%s %02x %d %d 100 %d _tmp.tap >> /dev/null", path, frequency,
                 channel_type - 1 ? (channel_type - 2 ? "stereoinv" : "stereo") : "mono",
@@ -118,43 +140,44 @@ int main(int argc, char *argv[])
         else
         {
             sprintf(command, "tmp.%s", ext);
-            ft = fopen(command, "rb");
-            if (ft)
+            if ((tmpFile = fopen(command, "rb")))
             {
                 if (tzx)
-                    fseek(ft, 0, SEEK_END),
-                        i = ftell(ft) - 10,
-                        fseek(ft, 10, SEEK_SET);
+                    fseek(tmpFile, 0, SEEK_END),
+                        i = ftell(tmpFile) - 10,
+                        fseek(tmpFile, 10, SEEK_SET);
                 else
-                    fread(mem, 1, 44, ft),
-                        i = *(uint32_t *)(mem + 40);
-                j = i >> 16;
-                k = i & 0xffff;
-                for (i = 0; i < j; i++)
-                    fread(in, 1, 0x10000, ft),
-                        fwrite(in, 1, 0x10000, fo);
-                fread(in, 1, k, ft);
-                fwrite(in, 1, k, fo);
-                fclose(ft);
+                {
+                    if (fread(mem, 1, 44, tmpFile) != 44)
+                        printf("Error: Reading wav header\n"),
+                            exit(-1);
+                    i = *(uint32_t *)(mem + 40);
+                }
+                while (i > 0)
+                {
+                    int l = fread(in, 1, 0x10000, tmpFile);
+                    fwrite(in, 1, l, outFile);
+                    i -= l;
+                }
+                fclose(tmpFile);
             }
             else
-                printf("\nError: plug doesn't generate valid file\n"),
+                perror(command),
                     exit(-1);
         }
         tlength -= 2 + length;
     }
-    if (remove(command) || remove("_tmp.tap"))
-        printf("\nError: deleting \n"),
-            exit(-1);
+    remove(command);
+    remove("_tmp.tap");
     remove("nul");
-    if (wav)
+    if (!tzx)
     {
-        uint32_t len = ftell(fo) - 8;
-        fseek(fo, 4, SEEK_SET);
-        fwrite(&len, sizeof(len), 1, fo);
+        uint32_t len = ftell(outFile) - 8;
+        fseek(outFile, 4, SEEK_SET);
+        fwrite(&len, sizeof(len), 1, outFile);
         len -= 36;
-        fseek(fo, 40, SEEK_SET);
-        fwrite(&len, sizeof(len), 1, fo);
+        fseek(outFile, 40, SEEK_SET);
+        fwrite(&len, sizeof(len), 1, outFile);
     }
-    fclose(fo);
+    fclose(outFile);
 }
--- src/leches.c
+++ src/leches.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,8 +15,9 @@ unsigned char termin[][8] =
     {{21, 22, 23, 24, 23, 24, 25, 26},
      {13, 14, 15, 16, 15, 16, 17, 18}};
 unsigned char *mem, *precalc;
+char *ext;
 unsigned char inibit = 0, tzx = 0, channel_type = 1, checksum, mlow, velo, refconf;
-FILE *fi, *fo;
+FILE *inFile, *outFile;
 int i, j, k, flag, ind = 0;
 unsigned short length, outbyte = 1, frequency, pilotts, pilotpulses;
 
@@ -38,15 +40,15 @@ void outbits(short val)
             else if (channel_type == 6)
                 precalc[ind++] = inibit ? 0x40 : 0xc0;
         }
-    if (ind > 0xff000)
-        fwrite(precalc, 1, ind, fo),
+    if (ind >= 0x10000)
+        fwrite(precalc, 1, ind, outFile),
             ind = 0;
     inibit ^= 1;
 }
 
 char char2hex(char value, char *name)
 {
-    if (value < '0' || value > 'f' || value < 'A' && value > '9' || value < 'a' && value > 'F')
+    if (!isxdigit(value))
         printf("\nInvalid character %c or '%s' not exists\n", value, name),
             exit(-1);
     return value > '9' ? 9 + (value & 7) : value - '0';
@@ -74,7 +76,9 @@ int parseHex(char *name, int index)
 int main(int argc, char *argv[])
 {
     mem = (unsigned char *)malloc(0x20000);
-    if (argc == 1)
+
+    if (argc != 10)
+    {
         printf("\n"
                "leches v0.03, an ultra load block generator by Antonio Villena, 17 Sep 2014\n\n"
                "  leches <srate> <channel_type> <ofile> <flag> <pilot_ms> <pause_ms> <ifile>\n\n"
@@ -88,13 +92,17 @@ int main(int argc, char *argv[])
                "  <pause_ms>      Duration of pause after block in milliseconds\n"
                "  <ifile>         Hexadecimal string or filename as data origin of that block\n\n"),
             exit(0);
-    if (argc != 10)
-        printf("\nInvalid number of parameters\n"),
-            exit(-1);
+    }
+
     frequency = atoi(argv[1]);
     if (frequency != 44100 && frequency != 48000)
         printf("\nInvalid sample rate: %d\n", frequency),
             exit(-1);
+
+    if (!(ext = strrchr(argv[3], '.')))
+        printf("%s: Output file name must have .wav or .tzx extension\n", argv[3]),
+            exit(-1);
+
     if (!strcasecmp(argv[2], "mono"))
         channel_type = 1;
     else if (!strcasecmp(argv[2], "stereo"))
@@ -104,14 +112,15 @@ int main(int argc, char *argv[])
     else
         printf("\nInvalid argument name: %s\n", argv[2]),
             exit(-1);
-    fo = fopen(argv[3], "wb+");
-    if (!fo)
+
+    if ((outFile = fopen(argv[3], "wb+")) == NULL)
         printf("\nCannot create output file: %s\n", argv[3]),
             exit(-1);
+
     precalc = (unsigned char *)malloc(0x200000);
-    if (!strcasecmp((char *)strchr(argv[3], '.'), ".tzx"))
-        ++tzx;
-    else if (!strcasecmp((char *)strchr(argv[3], '.'), ".wav"))
+    if (!strcasecmp(ext, ".tzx"))
+        tzx = 1;
+    else if (!strcasecmp(ext, ".wav"))
     {
         memset(mem, 0, 44);
         memset(precalc, 128, 0x200000);
@@ -125,7 +134,7 @@ int main(int argc, char *argv[])
         *(uint32_t *)(mem + 28) = frequency * (channel_type & 3);
         *(uint8_t *)(mem + 34) = 8;
         *(uint32_t *)(mem + 36) = 0x61746164;
-        fwrite(mem, 1, 44, fo);
+        fwrite(mem, 1, 44, outFile);
     }
     else
         printf("Output format not allowed, use only TZX or WAV\n"),
@@ -133,25 +142,24 @@ int main(int argc, char *argv[])
     mlow = frequency == 48000 ? 1 : 0;
     pilotts = mlow ? 875 : 952;
     pilotpulses = atof(argv[7]) * 3500 / pilotts + 0.5;
-    pilotpulses & 1 || ++pilotpulses;
-    fi = fopen(argv[9], "rb");
-    if (fi)
-        length = fread(mem, 1, 0x20000, fi);
+    inFile = fopen(argv[9], "rb");
+    if (inFile)
+        length = fread(mem, 1, 0x20000, inFile);
     else
         length = parseHex(argv[9], 0);
     velo = atoi(argv[5]);
-    refconf = (byvel[mlow][velo] & 128) + (byvel[mlow][velo] + 3 * atoi(argv[6]) & 127);
+    refconf = (byvel[mlow][velo] & 128) + ((byvel[mlow][velo] + 3 * atoi(argv[6])) & 127);
     for (checksum = i = 0; i < length; i++)
         checksum ^= mem[i];
     if (tzx)
-        fprintf(fo, "ZXTape!"),
-            *(int *)precalc = 0xa011a,
+        fprintf(outFile, "ZXTape!"),
+            *(uint32_t *)precalc = 0xa011a,
                   precalc[3] = 0x12,
-                  *(short *)(precalc + 4) = pilotts,
-                  *(short *)(precalc + 6) = pilotpulses,
+                  *(uint16_t *)(precalc + 4) = pilotts,
+                  *(uint16_t *)(precalc + 6) = pilotpulses,
                   precalc[8] = 0x15,
-                  *(short *)(precalc + 9) = mlow ? 73 : 79,
-                  *(short *)(precalc + 11) = atof(argv[8]),
+                  *(uint16_t *)(precalc + 9) = mlow ? 73 : 79,
+                  *(uint16_t *)(precalc + 11) = atof(argv[8]),
                   ind = 17;
     else
         while (pilotpulses--)
@@ -187,24 +195,24 @@ int main(int argc, char *argv[])
         for (j = 8; outbyte < 0x100; outbyte <<= 1, --j)
             ;
         precalc[ind++] = outbyte;
-        fwrite(precalc, 1, ind, fo);
-        i = j | (ftell(fo) - 24) << 8;
-        fseek(fo, 20, SEEK_SET);
-        fwrite(&i, 4, 1, fo);
+        fwrite(precalc, 1, ind, outFile);
+        i = j | (ftell(outFile) - 24) << 8;
+        fseek(outFile, 20, SEEK_SET);
+        fwrite(&i, 4, 1, outFile);
     }
     else
     {
-        fwrite(precalc, 1, ind, fo);
-        fwrite(precalc + 0x100000, 1, frequency * (channel_type & 3) * atof(argv[8]) / 1000, fo);
-        uint32_t len = ftell(fo) - 8;
-        fseek(fo, 4, SEEK_SET),
-            fwrite(&len, sizeof(len), 1, fo),
+        fwrite(precalc, 1, ind, outFile);
+        fwrite(precalc + 0x100000, 1, frequency * (channel_type & 3) * atof(argv[8]) / 1000, outFile);
+        uint32_t len = ftell(outFile) - 8;
+        fseek(outFile, 4, SEEK_SET),
+            fwrite(&len, sizeof(len), 1, outFile),
             len -= 36,
-            fseek(fo, 40, SEEK_SET),
-            fwrite(&len, sizeof(len), 1, fo);
+            fseek(outFile, 40, SEEK_SET),
+            fwrite(&len, sizeof(len), 1, outFile);
     }
-    fclose(fi);
-    fclose(fo);
+    fclose(inFile);
+    fclose(outFile);
     printf("\nFile generated successfully\n");
     exit(0);
 }
